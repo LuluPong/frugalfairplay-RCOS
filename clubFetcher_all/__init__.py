0803902e3580c582ed77cfa82350b3527ed5d5e8
LuluPong <frempl@rpi.edu>
2024-02-02 16:59:08 -0500

    Added files for python

---
+++ clubFetcher_all/__init__.py
@@ -0,0 +1,26 @@
+import datetime
+import logging
+
+import httpx
+
+import azure.functions as func
+
+class ClubFetcher:
+    def __init__(self) -> None:
+
+        pass
+    
+    def getAllClubs(self):
+        page = httpx.get("https://fbref.com/en/comps/Big5/Big-5-European-Leagues-Stats").text
+        return page
+        
+
+
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
